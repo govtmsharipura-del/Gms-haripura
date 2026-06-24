b53563a9974c1f0fe7db705a1e55109e432071e9
govtmsharipura-del <govtmsharipura@gmail.com>
2026-06-24 13:22:14 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'gms-haripura-v1';
+const urlsToCache = [
+  'https://msharipura.blogspot.com/p/home.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
